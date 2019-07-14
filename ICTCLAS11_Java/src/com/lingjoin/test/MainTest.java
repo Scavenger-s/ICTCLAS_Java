c2c143fe4914c7991416770f50c96ba21845a39f
Scavenger-s <1106042046@qq.com>
2019-07-14 10:34:58 +0800

    提交

---
+++ ICTCLAS11_Java/src/com/lingjoin/test/MainTest.java
@@ -0,0 +1,21 @@
+package com.lingjoin.test;
+
+import java.io.BufferedReader;
+import java.io.*;
+
+import com.lingjoin.demo.NlpirLib;
+
+public class MainTest {
+    static int i=0;
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		NlpirTest t=new NlpirTest();
+		t.testParagraphProcess();
+		
+	    //t.testFileProcess();
+//		t.testImportUserDict();
+//		t.SaveTheUsrDic();	
+//		t.Exit();
+	}
+}
+
